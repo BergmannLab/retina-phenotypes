6c120fc91beba30f34fff26a4e7ba5c24e4ee7b0
mjbeyeler <mjbeyeler@protonmail.ch>
2022-05-06 17:28:20 +0200

    Resolves #4

--- configs/config_.sh
+++ configs/config_.sh
@@ -2,7 +2,7 @@
 
 config_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# quality thresholds for ARIA
+#### quality thresholds for ARIA
 min_QCthreshold_1=0
 max_QCthreshold_1=9000000
 min_QCthreshold_2=0
@@ -11,5 +11,5 @@ max_QCthreshold_2=100000
 #### QUALITY THRESHOLDS OF LWNET ARTERY/VEIN CLASSIFICATION: 
 AV_threshold=0.79 # Add comment
 
-# configurations to adapt by user
+#### configurations to adapt by user
 source $config_dir/config_personal.sh

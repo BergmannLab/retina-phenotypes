af510afcc68090772cd8b3ca3aed6f8c4b55a6b9
mjbeyeler <mjbeyeler@protonmail.ch>
2022-05-30 16:15:25 +0200

    tva taa fix

--- preprocessing/measurePhenotype.py
+++ preprocessing/measurePhenotype.py
@@ -399,8 +399,8 @@ def read_data(imageID, diameter=False):
 
     :return:
     """
-    x = get_data_unpivot(f"{aria_measurements_dir}/{imageID}_all_center1Coordinates.tsv")
-    y = get_data_unpivot(f"{aria_measurements_dir}/{imageID}_all_center2Coordinates.tsv")
+    x = get_data_unpivot(f"{aria_measurements_dir}/{imageID}_all_center2Coordinates.tsv")
+    y = get_data_unpivot(f"{aria_measurements_dir}/{imageID}_all_center1Coordinates.tsv")
     df_all_seg = pd.read_csv(f"{aria_measurements_dir}/{imageID}_all_segmentStats.tsv", sep='\t')
     df_all_seg.reset_index(inplace=True)
     if diameter:

4ad1c19f0cf1185f756eb64593a8e54897a7a9af
sortinve <sofia.ortinvela@unil.ch>
2022-11-24 10:25:54 +0100

    Make sure when instance is null, the cov with both are null. Problem with nans nulls

--- complementary/image_to_participant/disease_cov_csv.py
+++ complementary/image_to_participant/disease_cov_csv.py
@@ -14,16 +14,21 @@ list_interest= ['age_center', 'assessment_centre', 'ethnic_background', 'date_ce
                  'eye_amblyopia', 'eye_presbyopia', 'eye_hypermetropia', 'eye_myopia', 'eye_astigmatism' ,'eye_diabetes', 'age_other_serious_eye_condition',
                  'age_pulmonary_embolism','spherical_power', 'cylindrical_power']
 
+
 for disease_name in list_interest:
-    disease_name_aux = f'{disease_name}_aux'
+    #disease_name_aux = f'{disease_name}_aux'
     disease_name_both = f'{disease_name}_both'
     disease_name_00 = f'{disease_name}_00'
     disease_name_10 = f'{disease_name}_10'
     #df_diseases_cov[disease_name_both] = np.where(df_diseases_cov["instance"] ==  1, df_diseases_cov[disease_name_10], df_diseases_cov[disease_name_00])
-    df_diseases_cov[disease_name_aux] = np.where(df_diseases_cov["instance"] == 0, df_diseases_cov[disease_name_00], df_diseases_cov[disease_name_10])
+    df_diseases_cov[disease_name_both] = np.where(df_diseases_cov["instance"] == 0, df_diseases_cov[disease_name_00], df_diseases_cov[disease_name_10])
     ## If nan in the instance => we do not take the covariant into account
-    df_diseases_cov[disease_name_both] = np.where(df_diseases_cov["instance"] == np.nan, np.nan, df_diseases_cov[disease_name_aux])
-    df_diseases_cov = df_diseases_cov.drop(columns=disease_name_aux)
+    df_diseases_cov[disease_name_both] = np.where(df_diseases_cov["instance"].isnull(), np.nan, df_diseases_cov[disease_name_both])
+    #df_diseases_cov = df_diseases_cov.drop(columns=disease_name_aux)
+    print('df_diseases_cov["instance"].isna().sum()', df_diseases_cov["instance"].isnull().sum())
+    print(disease_name_both, df_diseases_cov[disease_name_both].isnull().sum())
+
+#print(df_diseases_cov['spherical_power_both'])
 
 df_diseases_cov['age_center_both_2']=df_diseases_cov['age_center_both']*df_diseases_cov['age_center_both']
 df_diseases_cov['spherical_power_both_2']=df_diseases_cov['spherical_power_both']*df_diseases_cov['spherical_power_both']
